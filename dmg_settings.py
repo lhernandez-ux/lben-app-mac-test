6b794072121fb3baf7e52f3d5c0bfed91e0970b5
Luisa Hernandez <lhernandez@e2energiaeficiente.com>
2026-05-19 09:02:45 -0500

    agrego build para Mac

---
+++ dmg_settings.py
@@ -0,0 +1,12 @@
+application = 'dist/LBEn_App.app'
+appname = 'LBEn App'
+format = 'UDZO'
+size = None
+files = [application]
+symlinks = {'Applications': '/Applications'}
+icon_locations = {
+    'LBEn App.app': (150, 150),
+    'Applications': (450, 150),
+}
+background = 'builtin-arrow'
+window_rect = ((100, 100), (600, 400))
